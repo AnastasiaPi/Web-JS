8dbd2e5d5c392d5255f9a76ab595933df98788e9
 Anastasia Pidgurska <anastasiapidgurska@MacBook-Pro-Anastasia.local>
2020-02-06 13:34:20 +0200

    New

--- JS-HOMEWORKS/JS-HW-1/Function/index.js
+++ JS-HOMEWORKS/JS-HW-1/Function/index.js
@@ -1,12 +1,32 @@
-let name= prompt("Your Name","Nastia" );
+let name= prompt("Your Name","Amigo" );
 let textHi = "Hi ";
 let textPrivet = "Privet ";
 let textBye = "Bye ";
-let changeName = function(yourName,hi){alert(hi+yourName)};
+let changeName = function(yourName,hi){
+    hi = hi+yourName
+    alert(hi)};
 function changeYourName(yourName,hi){
-    alert(hi+ yourName);
+    hi = hi+yourName
+    alert(hi);
 }
 var changeNameTwo = new Function('yourname,bye', ' confirm(bye+yourname); ');
+function MyArray(...args) {
+    for (i = 0; i < args.length; i++) {
+        if (args[i] == 1) { // Проверка на наличие числа 1 в массиве
+            return console.log("Число 1")
+        } ;
+    };
+  };
+function MyDefaultArray(a,r,g = 3) {
+    for (i = 0; i < arguments.length; i++) {
+        if (arguments[i] == 1) { // Проверка на наличие числа 1 в массиве
+            return console.log("Число 2")
+        }; 
+    };
+  };
+
+MyArray(4,1,6,0);
+MyDefaultArray(1,4);
 changeName(name,textHi);
 changeYourName(name,textPrivet);
 changeNameTwo(name, textBye)
--- JS-HOMEWORKS/JS-HW-1/String/index.js
+++ JS-HOMEWORKS/JS-HW-1/String/index.js
@@ -1,17 +1,19 @@
 let stroka= prompt("Your String","Alphabet" );
 let count = 0;
 let char = prompt("Your String","ABC" );
-stroka.toString.toLowerCase;
-char.toString.toLowerCase
 console.log(stroka);
 console.log("Letters: "+char)
-for(let iter of stroka){
-  for(let char_iter of char){
-    if(iter == char_iter){
-      count++;
-    };
-  };
+function Search_count(str, ch, ct){
+  str.toString.toLowerCase;
+  ch.toString.toLowerCase;
+  for(let iter of str){
+    for(let char_iter of ch){
+      if(iter == char_iter){
+        ct++;
+      };};};
+  return ct;
 };
+count = Search_count(stroka,char, count);
 console.log("Count:  "+count);
 alert("Count of same letter:   "+count);
   
